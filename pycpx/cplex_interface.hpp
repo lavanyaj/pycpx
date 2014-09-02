0f4cf0e4bed2a9f7dbf4eec178e01790aba34c58
lav <lav@lav-MacBookPro.(none)>
2014-09-02 14:57:16 -0700

    cplex status codes for MIP

--- pycpx/cplex_interface.hpp
+++ pycpx/cplex_interface.hpp
@@ -24,6 +24,39 @@ using namespace std;
 #define MODEL_INFEASABLE 3
 #define MODEL_UNBOUNDED_OR_INFEASABLE 4
 
+#define CPX_STAT_OPTIMAL 1
+#define CPX_STAT_UNBOUNDED 2
+#define CPX_STAT_INFEASIBLE 3
+#define CPX_STAT_INForUNBD 4
+#define CPX_STAT_OPTIMAL_INFEAS 5
+#define CPX_STAT_NUM_BEST 6
+#define CPX_STAT_ABORT_IT_LIM 10
+#define CPX_STAT_ABORT_TIME_LIM 11
+#define CPX_STAT_ABORT_OBJ_LIM 12
+#define CPX_STAT_ABORT_USER 13
+#define CPX_STAT_OPTIMAL_FACE_UNBOUNDED 20
+#define CPX_STAT_ABORT_PRIM_OBJ_LIM 21
+#define CPX_STAT_ABORT_DUAL_OBJ_LIM 22
+#define CPXMIP_OPTIMAL 101
+#define CPXMIP_OPTIMAL_TOL 102
+#define CPXMIP_INFEASIBLE 103
+#define CPXMIP_SOL_LIM 104
+#define CPXMIP_NODE_LIM_FEAS 105
+#define CPXMIP_NODE_LIM_INFEAS 106
+#define CPXMIP_TIME_LIM_FEAS 107
+#define CPXMIP_TIME_LIM_INFEAS 108
+#define CPXMIP_FAIL_FEAS 109
+#define CPXMIP_FAIL_INFEAS 110
+#define CPXMIP_MEM_LIM_FEAS 111
+#define CPXMIP_MEM_LIM_INFEAS 112
+#define CPXMIP_ABORT_FEAS 113
+#define CPXMIP_ABORT_INFEAS 114
+#define CPXMIP_OPTIMAL_INFEAS 115
+#define CPXMIP_FAIL_FEAS_NO_TREE 116
+#define CPXMIP_FAIL_INFEAS_NO_TREE 117
+#define CPXMIP_UNBOUNDED 118
+#define CPXMIP_INForUNBD 119
+
 ////////////////////////////////////////////////////////////////////////////////
 // Now the same for the model
 
@@ -173,9 +206,86 @@ public:
     return Status();
   }
 
+  Status handleCplexStatus(IloCplex::CplexStatus status) 
+  {
+    switch(status) {
+      /*
+    case IloCplex::Optimal:
+      return Status("Optimal solution is available", CPX_STAT_OPTIMAL);
+    case IloCplex::Unbounded:
+      return Status("Model has an Unbounded ray", CPX_STAT_UNBOUNDED);
+    case IloCplex::Infeasible:
+      return Status("Model is proved Infeasible", CPX_STAT_INFEASIBLE);
+    case IloCplex::InfOrUnbd:
+      return Status("Model is proved either Infeasible or Unbounded", CPX_STAT_INForUNBD);
+    case IloCplex::OptimalInfeas:
+      return Status("Optimal solution is available, but with infeasibilities after unscaling", CPX_STAT_OPTIMAL_INFEAS);
+    case IloCplex::NumBest:
+      return Status("Solution is available, but not proved optimal, due to numerical difficulties during optimization", CPX_STAT_NUM_BEST);
+    case IloCplex::AbortItLim:
+      return Status("Aborted due to an iteration limit", CPX_STAT_ABORT_IT_LIM);
+    case IloCplex::AbortTimeLim:
+      return Status("Aborted due to a time limit", CPX_STAT_ABORT_TIME_LIM);
+    case IloCplex::AbortObjLim:
+      return Status("Aborted due to an objective limit", CPX_STAT_ABORT_OBJ_LIM);
+    case IloCplex::AbortUser:
+      return Status("Aborted on user request", CPX_STAT_ABORT_USER);
+    case IloCplex::OptimalFaceUnbounded:
+      return Status("Model has Unbounded optimal face", CPX_STAT_OPTIMAL_FACE_UNBOUNDED);
+    case IloCplex::AbortPrimObjLim:
+      return Status("Aborted due to a primal obj limit", CPX_STAT_ABORT_PRIM_OBJ_LIM);
+    case IloCplex::AbortDualObjLim:
+      return Status("Aborted due to a dual obj limit", CPX_STAT_ABORT_DUAL_OBJ_LIM);  
+      */
+    case IloCplex::Optimal:
+      return Status();
+      //"Optimal integer solution found", CPXMIP_OPTIMAL);
+    case IloCplex::OptimalTol:
+      return Status();
+      //"Optimal sol. within epgap or epagap tolerance found", CPXMIP_OPTIMAL_TOL);
+    case IloCplex::Infeasible:
+      return Status("Integer infeasible", CPXMIP_INFEASIBLE);
+    case IloCplex::SolLim:
+      return Status("Mixed integer solutions limit exceeded", CPXMIP_SOL_LIM);
+    case IloCplex::NodeLimFeas:
+      return Status("Node limit exceeded, integer solution exists", CPXMIP_NODE_LIM_FEAS);
+    case IloCplex::NodeLimInfeas:
+      return Status("Node limit exceeded, no integer solution", CPXMIP_NODE_LIM_INFEAS);
+    case IloCplex::AbortTimeLim:
+      return Status("Time limit exceeded, integer solution exists", CPXMIP_TIME_LIM_FEAS);
+      //case IloCplex::TimeLimInfeas:
+      //return Status("Time limit exceeded, no integer solution", CPXMIP_TIME_LIM_INFEAS);
+    case IloCplex::FailFeas:
+      return Status("Error termination, integer solution exists", CPXMIP_FAIL_FEAS);
+    case IloCplex::FailInfeas:
+      return Status("Error termination, no integer solution", CPXMIP_FAIL_INFEAS);
+    case IloCplex::MemLimFeas:
+      return Status("Treememory limit, integer solution exists", CPXMIP_MEM_LIM_FEAS);
+    case IloCplex::MemLimInfeas:
+      return Status("Treememory limit, no integer solution exists", CPXMIP_MEM_LIM_INFEAS);
+      //case IloCplex::AbortFeas:
+      //return Status("Aborted, integer solution exists", CPXMIP_ABORT_FEAS);
+      //case IloCplex::AbortInfeas:
+      //return Status("Aborted, no integer solution", CPXMIP_ABORT_INFEAS);
+    case IloCplex::OptimalInfeas:
+      return Status("Problem optimal with unscaled infeasibilities", CPXMIP_OPTIMAL_INFEAS);
+    case IloCplex::FailFeasNoTree:
+      return Status("Out of memory, no tree, integer solution exists", CPXMIP_FAIL_FEAS_NO_TREE);
+    case IloCplex::FailInfeasNoTree:
+      return Status("Out of memory, no tree, no integer solution", CPXMIP_FAIL_INFEAS_NO_TREE);
+    case IloCplex::Unbounded:
+      return Status("Model has an Unbounded ray", CPXMIP_UNBOUNDED);
+    case IloCplex::InfOrUnbd:
+      return Status("Model is proved either Infeasible or Unbounded", CPXMIP_INForUNBD);
+    default:
+      return Status("Unknown status code from Cplex");
+    }
+  }
+
   Status solve(IloNum * elapsed_time = NULL)
   {
 
+    bool unsolved = false;
     try{
       if(!model_extracted)
 	extractModel();
@@ -186,31 +296,30 @@ public:
       if(! solver.solve() )
 	{
 	  *elapsed_time = 0;
-
-	  IloAlgorithm::Status status = solver.getStatus();
-		    
-	  switch(status) {
-	  case IloAlgorithm::Unbounded:
-	    return Status("Model unbounded.", MODEL_UNBOUNDED);
-	  case IloAlgorithm::Infeasible:
-	    return Status("Model infeasible.", MODEL_INFEASABLE);
-	  case IloAlgorithm::InfeasibleOrUnbounded:
-	    return Status("Model unbounded or infeasable.", MODEL_UNBOUNDED_OR_INFEASABLE);
-	  default:
-	    return Status("Unknown error occured while solving model.");
-	  }
+	  unsolved = true;
 	}
 
       if(elapsed_time != NULL)
 	*elapsed_time += solver.getImpl()->getCplexTime();
+
     }
     catch(IloException& e){
       return Status(e.getMessage());
     }
-	    
-    model_solved = true;
+    
+    if (unsolved != true)
+      model_solved = true;
+    
+    try
+      {
+	IloCplex::CplexStatus cplexStatus = solver.getCplexStatus();
+	return handleCplexStatus(cplexStatus);
+      }
+    catch(IloException& e){
+      return Status(e.getMessage());
+    }
+    
 
-    return Status();
   }
 
   Status readBasis(const char* filename)
